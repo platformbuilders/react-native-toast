e18379f53d365c1f4a02a9be864fcb9ef9f3efc1
luancurti <luancurti@gmail.com>
2023-06-06 09:35:26 -0300

    fix(typings): declare types explict in module

--- react-native-toast.d.ts
+++ react-native-toast.d.ts
@@ -4,15 +4,70 @@ declare module "@platformbuilders/react-native-toast" {
     config?: ToastConfigProps;
   }
 
-  export type ToastType = import('./src/Toast').ToastType;
+  export type ToastType = 'success' | 'alert' | 'warning' | 'custom';
 
-  export type ContextType = import('./src/ToastProvider').ContextType;
+  export type ContextType = {
+    showToast(data: ToastProps): void;
+  };
 
-  export type ToastProps = import('./src/Toast').ToastProps;
+  export type ToastProps = {
+    title?: string | undefined;
+    message: string;
+    type: ToastType;
+    duration?: number;
+  };
 
-  export type PanGestureContextType = import('./src/Toast').PanGestureContextType;
+  export type PanGestureContextType = {
+    translateY: number;
+  };
 
-  export type ToastConfig = import('./src/Toast').ToastConfig;
+  export type ToastConfig = {
+    fontFamily?: string;
+    textColor?: string;
+    backgroundColor?: {
+      success?: string;
+      alert?: string;
+      warning?: string;
+      custom?: string;
+    };
+    icon: {
+      success: {
+        icon?: ImageSourcePropType;
+        height?: number;
+        width?: number;
+      };
+      alert: {
+        icon?: ImageSourcePropType;
+        height?: number;
+        width?: number;
+      };
+      warning: {
+        icon?: ImageSourcePropType;
+        height?: number;
+        width?: number;
+      };
+      custom: {
+        icon?: ImageSourcePropType;
+        height?: number;
+        width?: number;
+      };
+    };
+    autoHide: {
+      success: boolean;
+      alert: boolean;
+      warning: boolean;
+      custom: boolean;
+    };
+    showCloseButton: {
+      success: boolean;
+      alert: boolean;
+      warning: boolean;
+      custom: boolean;
+    };
+    showIcon?: boolean;
+  };
 
   export function useToast(): ContextType;
+
+  export function ToastProvider({ children, config }: ToastProviderProps): React.JSX.Element
 }
